f6dd7249878f534ce9069f2e0defeab897ec9e89
ezhayog <charlie.zha@ericsson.com>
2018-10-09 08:31:55 +0200

    example secerty: use struc to hold listener

--- src/github.com/zysimplelife/secret/server/tlsServer.go
+++ src/github.com/zysimplelife/secret/server/tlsServer.go
@@ -12,6 +12,79 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
+type ReloadableListener struct {
+	done chan bool
+	cert string
+	key  string
+	ln   net.Listener
+}
+
+func (rl ReloadableListener) loadCertificate() (tlsConfig *tls.Config, err error) {
+	cer, err := tls.LoadX509KeyPair(rl.cert, rl.key)
+	if err != nil {
+		log.Println("some error happened ", err)
+	}
+
+	log.Println("size in the certificate chain =", len(cer.Certificate))
+
+	cerX509, err := x509.ParseCertificate(cer.Certificate[0])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("certificate is for ", cerX509.Subject)
+	log.Println("certificate issuer is  ", cerX509.Issuer)
+	log.Println("IP Addresse is ", cerX509.IPAddresses)
+
+	tlsConfig = &tls.Config{Certificates: []tls.Certificate{cer}}
+	return
+}
+
+func (rl *ReloadableListener) serve() {
+	log.Println("Starting server")
+	config, err := rl.loadCertificate()
+	if err != nil {
+		log.Println("some error happened ", err)
+	}
+	//serve incoming request
+	rl.ln, err = tls.Listen("tcp", ":7443", config)
+	if err != nil {
+		log.Println("some error happened ", err)
+	}
+	rl.done = make(chan bool, 1)
+	go func() {
+		for {
+			log.Println("Start accepting request with addr", rl.ln.Addr)
+			conn, err := rl.ln.Accept()
+			if err != nil {
+				log.Println("Some error happened", err)
+				break
+			}
+			go handleConnection(conn)
+		}
+		log.Println("stop serving,release listener ", rl.ln)
+		rl.done <- true
+	}()
+	log.Println("Serving started")
+}
+
+func (rl *ReloadableListener) stopServing() {
+	if rl.ln == nil {
+		log.Println("listener is nil, no need release")
+		return
+	}
+	rl.ln.Close()
+	<-rl.done
+	rl.ln = nil
+	log.Println("Serving stoped")
+}
+
+func (rl *ReloadableListener) restart() {
+	log.Println("Restarting server....")
+	rl.stopServing()
+	rl.serve()
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 	log.Println("Server is starting")
@@ -26,9 +99,10 @@ func main() {
 	}()
 
 	// start server
-	ln, _ := createListener()
-	done := make(chan bool, 1)
-	go serve(done, ln)
+	server := ReloadableListener{
+		cert: "./server.crt",
+		key:  "./server.key",
+	}
 
 	// watching certificate change
 	log.Println("Start receiving message from watcher")
@@ -37,10 +111,7 @@ func main() {
 	for {
 		select {
 		case <-needReload:
-			log.Println("Restarting server....")
-			stopServing(done, ln)
-			ln, _ = createListener()
-			go serve(done, ln)
+			server.restart()
 		}
 	}
 
@@ -83,61 +154,12 @@ func waitUntilDone(watcher *fsnotify.Watcher, needReload chan bool) {
 		case <-watcher.Events:
 		case <-watcher.Errors:
 		case <-timer.C:
-			log.Println("no new event update, file change is done.")
+			log.Println("file change is done.")
 			needReload <- true
 			return
 		}
 		timer.Stop()
 	}
-}
-
-func createListener() (ln net.Listener, err error) {
-	cer, err := tls.LoadX509KeyPair("./server.crt", "./server.key")
-	if err != nil {
-		log.Println("some error happened ", err)
-	}
-
-	log.Println("size in the certificate chain =", len(cer.Certificate))
-
-	cerX509, err := x509.ParseCertificate(cer.Certificate[0])
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("certificate is for ", cerX509.Subject)
-	log.Println("certificate issuer is  ", cerX509.Issuer)
-	log.Println("IP Addresse is ", cerX509.IPAddresses)
-
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err = tls.Listen("tcp", ":7443", config)
-	if err != nil {
-		log.Println("some error happened ", err)
-	}
-	return
-}
-
-func serve(done chan bool, ln net.Listener) {
-	//serve incoming request
-	log.Println("Serving started")
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Println("Some error happened", err)
-			break
-		}
-		go handleConnection(conn)
-	}
-	log.Println("stop serving,release listener ", ln)
-	done <- true
-}
-
-func stopServing(done chan bool, ln net.Listener) {
-	if ln == nil {
-		log.Println("listener is nil, no need release")
-	}
-	ln.Close()
-	<-done
-	log.Println("Serving stoped")
 }
 
 func handleConnection(conn net.Conn) {
