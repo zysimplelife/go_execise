f7f45d31448aa264548bb87252893cc2e898fd32
ezhayog <charlie.zha@ericsson.com>
2019-02-07 10:10:12 +0100

    add kafka examples

--- kafka/client.go
+++ kafka/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/Shopify/sarama"
@@ -11,35 +12,31 @@ func main() {
 	// Because we don't change the flush settings, sarama will try to produce messages
 	// as fast as possible to keep latency low.
 	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
-	config.Producer.Retry.Max = 10                   // Retry up to 10 times to produce the message
+	config.Producer.RequiredAcks = sarama.WaitForLocal // Wait for all in-sync replicas to ack the message
+	config.Producer.Retry.Max = 10                     // Retry up to 10 times to produce the message
 	config.Producer.Return.Successes = true
-	tlsConfig := createTlsConfiguration()
-	if tlsConfig != nil {
-		config.Net.TLS.Config = tlsConfig
-		config.Net.TLS.Enable = true
-	}
 
 	// On the broker side, you may want to change the following settings to get
 	// stronger consistency guarantees:
 	// - For your broker, set `unclean.leader.election.enable` to false
 	// - For the topic, you could increase `min.insync.replicas`.
 
-	producer, err := sarama.NewSyncProducer("127.0.0.1:9092", config)
+	broker1 := "localhost:39093"
+	producer, err := sarama.NewSyncProducer([]string{broker1}, config)
 	if err != nil {
 		log.Fatalln("Failed to start Sarama producer:", err)
 	}
 
 	partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
-		Topic: "important",
+		Topic: "test",
 		Value: sarama.StringEncoder("I have send a message"),
 	})
 
 	if err != nil {
-		fmt.printf("Failed to store your data:, %s", err)
+		fmt.Printf("Failed to store your data:, %s", err)
 	} else {
 		// The tuple (topic, partition, offset) can be used as a unique identifier
 		// for a message in a Kafka cluster.
-		fmt.printf("Your data is stored with unique identifier important/%d/%d", partition, offset)
+		fmt.Printf("Your data is stored with unique identifier important/%d/%d", partition, offset)
 	}
 }
